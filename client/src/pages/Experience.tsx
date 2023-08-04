9aa7232d94379f12e4036fa7674e4ae39b50099d
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-08-04 10:56:56 +0530

    data fetched from database using axios

--- client/src/pages/Experience.tsx
+++ client/src/pages/Experience.tsx
@@ -1,11 +1,24 @@
-import ExperienceData from "../constants/ExperienceData"
+import { useEffect, useState } from "react"
+import axios from "axios"
+import { ExperienceItem } from "../types/FileTypes"
 
 const Experience = () => {
+  const [experienceData, setExperienceData] = useState<ExperienceItem[]>([]) 
+
+  useEffect(() => {
+    axios.get("http://localhost:3002/api/getExperience").then((res) => {
+      setExperienceData(res.data)
+      // console.log("inner =>", res.data)
+    }).catch((err) => {
+      console.log(err)
+    })
+  }, [])
+
   return (
     <div style={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "flex-start", width: "100%", gap: "2rem"}}>
       
       {/* <div style={{width: "100%"}}> */}
-        {ExperienceData.map((experience, index) => {
+        {experienceData.map((experience, index) => {
             return (
                 <div key={index} style={{width: "60%", border: "1px solid #d3d3d3", padding: "1rem 2rem"}}>
                     <img src={experience?.logoLink} alt={experience?.LogoName} height={80}/>
@@ -21,7 +34,7 @@ const Experience = () => {
                     <p>{experience?.position}</p>
                     <p>{experience?.startDate}</p>
                     <p>{experience?.endDate}</p>
-                    <p>{experience?.techstack?.map((items) => {
+                    <p>{experience?.techStack?.map((items) => {
                         return (
                             <p>{items}</p>
                         )
