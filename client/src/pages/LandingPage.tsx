cb23597e9b4dd6798b44e0555adf4101de86de22
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-08-05 13:10:44 +0530

    landing page section completed and about me section started

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,21 +1,50 @@
-import Footer from "../components/Footer"
-import MobileNavbar from "../components/MobileNavbar"
-import Navbar from "../components/Navbar"
-import Experience from "./Experience"
+import Footer from "../components/Footer";
+import MobileNavbar from "../components/MobileNavbar";
+import Navbar from "../components/Navbar";
+import SectionTitle from "../components/SectionTitle";
+import "../styles/LandingPage.css";
 
 const LandingPage = () => {
   return (
-    <div style={{display: "flex",flexDirection: "column", justifyContent: "space-between", height: "100vh"}}>
-        <div>
+    <div>
+      <Navbar />
+      <div style={{width: "100%", background: "#f4f4f4"}}>
 
-            <div><Navbar/></div>
-            <div style={{display: "flex", flexDirection: "column", margin: "5rem 0"}}><Experience/></div>
-        
-            <div><MobileNavbar/></div>
+
+      {/* --------------  Landing Section  ------------ */}
+        <div style={{width: "100%", background: "linear-gradient(to right, transparent 50%, red 50%)", display: "flex", alignItems: "center", justifyContent: "center", height: "100vh", }}>
+          <div style={{ display: "flex", flexDirection: 'row',width: "1280px"}}>
+            <div style={{display: "flex", flexDirection: "column", width: "50%"}}>
+              <p className="iii" style={{fontSize: "6rem"}}>Hey, I'm</p>
+              <h1 className="title">Sayan</h1>
+              {/* <p style={{fontSize: "2rem", fontStyle: "italic", color: "#c7c7c7"}}> A passionate Frontend & aspiring Full Stack developer from India</p> */}
+              <p style={{fontSize: "2rem", fontStyle: "italic", color: "#c7c7c7"}}> from sketches to code, bringing designs to life with passion</p>
+            </div>
+            
+            {/* <div className="home__image" style={{display: "flex", width: "50%"}}>
+                <div className="home__solid"></div>
+                <div className="home__frame"></div>
+                <div className="home__stripe"></div>
+                <div className="home__pic"></div>
+              </div> */}
+          </div>
         </div>
-      <div><Footer/></div>
-    </div>
-  )
-}
 
-export default LandingPage
+
+        {/* --------------  About Me  ------------ */}
+        <SectionTitle mainTitle={"About Me"} subTitle={"A little Flex ."}/>
+        <section className="aboutMe"  style={{ display: "flex", flexDirection: 'column',width: "100%", alignItems: "center"}}>
+          <div className="outerDiv-aboutMe" style={{ display: "flex", flexDirection: 'column',width: "1280px", alignItems: "center"}}>
+              
+          </div>
+        </section>
+        
+
+      </div>
+      <MobileNavbar />
+      <Footer /> 
+    </div>
+  );
+};
+
+export default LandingPage;
