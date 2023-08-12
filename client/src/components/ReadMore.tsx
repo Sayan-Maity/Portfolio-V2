f577c55daca1c1d9c53b6dce6f239808689e3136
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-08-13 00:12:22 +0530

    readmore component is created for shortening the length of text

---
+++ client/src/components/ReadMore.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { IReadMoreProps } from "../types/PropTypes";
+
+const ReadMore = ({ text, maxCharacters, name }: IReadMoreProps) => {
+  const [showFullText, setShowFullText] = useState(false);
+
+
+  const toggleTextVisibility = () => {
+    setShowFullText(!showFullText);
+  };
+
+  const renderedText = showFullText ? text : text.slice(0, maxCharacters);
+
+  return (
+    <div style={{ display: "flex" }}>
+      <p>
+        {renderedText}
+        {text.length > maxCharacters && (
+          <>
+            {showFullText ? null : <span>...</span>}
+            <button
+              style={{
+                border: "none",
+                backgroundColor: "transparent",
+                padding: "0 0.5rem",
+                fontWeight: "bold",
+                cursor: "pointer",
+                color: "var(--color-dark)",
+                fontSize: "1.1rem",
+              }}
+              onClick={toggleTextVisibility}
+            >
+              {showFullText ? "read less" : "read more"}
+            </button>
+          </>
+        )}
+      </p>
+    </div>
+  );
+};
+
+export default ReadMore;
