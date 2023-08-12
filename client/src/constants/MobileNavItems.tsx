deaa51e2d50d3d30f7b4aa174611533a1e462e08
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-08-13 00:16:36 +0530

    mobileNavItems are stored here

---
+++ client/src/constants/MobileNavItems.tsx
@@ -0,0 +1,29 @@
+const mobileNavItems = [
+    {
+        url: "/",
+        icon: "AiOutlineHome",
+        label: "Home"
+    },
+    {
+        url: "/projects",
+        icon: "AiOutlineFundProjectionScreen",
+        label: "Projects"
+    },
+    {
+        url: "/experience",
+        icon: "BsBriefcase",
+        label: "Experience"
+    },
+    {
+        url: "/blogs",
+        icon: "BsPencilSquare",
+        label: "Blogs"
+    },
+    {
+        url: "/contact",
+        icon: "AiOutlineMessage",
+        label: "Contact"
+    },
+]
+
+export default mobileNavItems
