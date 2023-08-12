42144f5348b1cbcd2315997fb162d7a0b2c6c089
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-08-13 01:24:23 +0530

    landing page is created

---
+++ admin/src/pages/LandingPage.tsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+      <Navbar />
+
+    </div>
+  );
+};
+
+export default LandingPage;
