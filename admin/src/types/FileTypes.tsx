d149bfeed20e98f9ddd720e5b6613c34c0e42e04
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-08-13 01:41:11 +0530

    all the interfaces of all the files are mentioned here

---
+++ admin/src/types/FileTypes.tsx
@@ -0,0 +1,10 @@
+
+export interface MessageItem {
+    firstName: string;
+    lastName: string;
+    subject: string;
+    email: string;
+    message: string;
+    createdAt: string;
+    updatedAt: string;
+  }
