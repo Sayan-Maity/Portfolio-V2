f8db4311e886461961d14b0961c95dc662b0b951
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-08-12 23:30:21 +0530

    blog page is updated and made responsive

--- client/src/pages/Blogs.tsx
+++ client/src/pages/Blogs.tsx
@@ -1,30 +1,37 @@
 import Footer from "../components/Footer";
+import FooterGap from "../components/FooterGap";
 import MobileNavbar from "../components/MobileNavbar";
 import Navbar from "../components/Navbar";
+import ParallaxComponent from "../components/ParallaxComponent";
+import "../styles/BlogPage.css";
+import ComingSoon from "../assets/comingSoon.webp";
+import SEO from "../components/SEO";
 
 const Contact = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
-        height: "100vh",
-      }}
-    >
-      <div>
-        <div>
-          <Navbar />
-        </div>
-        <h1>Blogs</h1>
-
-        <div>
-          <MobileNavbar />
+    <div>
+      <Navbar />
+      <SEO dynamicTitle="Sayan | Blogs" />
+      <div style={{ width: "100%", background: "#f4f4f4" }}>
+        <ParallaxComponent
+          title="Blogs"
+          wallpaper="https://ik.imagekit.io/sayancr777/tr:w-1500/Portfolio/blogBanner.png?updatedAt=1691399103917"
+        />
+        <div
+          className="aurora-BlogDiv"
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            padding: "5rem 0",
+          }}
+        >
+          <img src={ComingSoon} alt="coming-soon" />
         </div>
       </div>
-      <div>
-        <Footer />
-      </div>
+      <MobileNavbar />
+      {/* <FooterGap/> */}
+      <Footer />
     </div>
   );
 };
