afd1c252571dbc3b98fd8da28f5a6ea96bfa08b6
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-08-12 23:26:48 +0530

    all the interfaces of the prop types are stored here

--- client/src/types/PropTypes.tsx
+++ client/src/types/PropTypes.tsx
@@ -1,4 +1,21 @@
 export interface ISectionTitleProps {
     mainTitle: string;
-    subTitle: string;
+    summary: string;
+}
+
+export interface IParallaxProps {
+    title: string;
+    wallpaper: string;
+}
+
+export interface IButtonProps {
+    name: string;
+}
+export interface ISeoProps {
+    dynamicTitle: string;
+}
+
+export interface IReadMoreProps {
+    text: string;
+    maxCharacters: number;
 }
