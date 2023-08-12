616a7ce38c87a723fc1d0d236f81b71493fa0cdd
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-08-12 23:21:47 +0530

    all the main navbar items are stored here

--- client/src/constants/NavItems.tsx
+++ client/src/constants/NavItems.tsx
@@ -1,8 +1,24 @@
 const navItems = [
     {
         url: "/",
+        label: "Home"
+    },
+    {
+        url: "/projects",
+        label: "Projects"
+    },
+    {
+        url: "/experience",
         label: "Experience"
-    }
+    },
+    {
+        url: "/blogs",
+        label: "Blogs"
+    },
+    {
+        url: "/contact",
+        label: "Contact"
+    },
 ]
 
 export default navItems
