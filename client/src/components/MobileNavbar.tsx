68e57c492e85ecaeb9e07c6c3198a9c9090c8843
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-08-12 23:02:56 +0530

    mobileNavbar is updated and made responsive

--- client/src/components/MobileNavbar.tsx
+++ client/src/components/MobileNavbar.tsx
@@ -1,20 +1,62 @@
-import React from "react";
+import { AiOutlineHome, AiOutlineMessage, AiOutlineFundProjectionScreen } from "react-icons/ai";
+import { BsBriefcase, BsPencilSquare } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import MobileNavItems from "../constants/MobileNavItems";
+import "../styles/MobileNavbar.css";
+import { MobileIconComponentsItem } from "../types/FileTypes";
+
+const iconComponents: MobileIconComponentsItem = {
+  AiOutlineHome,
+  AiOutlineFundProjectionScreen,
+  BsBriefcase,
+  BsPencilSquare,
+  AiOutlineMessage,
+};
 
 const MobileNavbar = () => {
   return (
-    <div>
+    <div
+      className="mobile-navbar"
+      style={{
+        position: "fixed",
+        left: "0",
+        bottom: "0",
+        width: "100%",
+        backgroundColor: "var(--color-white)",
+        padding: "0.5rem 1rem",
+        boxShadow: "0px -2px 10px #dedede",
+      }}
+    >
       <div
-        className="mobile-navbar"
         style={{
-          position: "fixed",
-          left: "0",
-          bottom: "0",
-          width: "100%",
-          backgroundColor: "yellow",
-          padding: "0.5rem 1rem",
+          display: "flex",
+          justifyContent: "space-between",
+          flexDirection: "row",
+          gap: "1rem",
         }}
       >
-        <h1>Below Navbar</h1>
+        {MobileNavItems.map((item, index: number) => {
+          const IconComponent = iconComponents[item.icon];
+          return (
+            <div key={index}>
+              <Link
+                to={item.url}
+                key={index}
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  fontSize: "1rem",
+                  textDecoration: "none",
+                  gap: "0.3rem",
+                }}
+              >
+                <IconComponent style={{fontSize: "1.2rem", color: "var(--font-color-dark)"}}/>
+                <p className="mobileNavItem-label" style={{ fontSize: "0.7rem" }}>{item.label}</p>
+              </Link>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
