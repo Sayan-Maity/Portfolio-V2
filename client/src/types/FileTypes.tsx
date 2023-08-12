33683537f353acef06b8c73583eabe3f4f7630c8
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-08-12 23:26:26 +0530

    all the interfaces of the file types are stored here

--- client/src/types/FileTypes.tsx
+++ client/src/types/FileTypes.tsx
@@ -1,13 +1,96 @@
+import { IconType } from "react-icons";
+
 export interface ExperienceItem {
-    logoLink: string;
-    LogoName: string;
-    companyName: string;
-    experienceDetails: string[];
-    location: string;
-    status: string;
-    role: string;
-    position: string;
-    startDate: string;
-    endDate: string;
-    techStack: string[];
-  }
+  logoLink: string;
+  LogoName: string;
+  companyName: string;
+  experienceDetails: string[];
+  location: string;
+  status: string;
+  role: string;
+  position: string;
+  startDate: string;
+  endDate: string;
+  techStack: string[];
+}
+
+export interface EducationItem {
+  name: string;
+  degree: string;
+  grade: string;
+  websiteLink: string;
+  major: string;
+  skills: string[];
+  educationDetails: string[];
+  activities: string[];
+  startDate: string;
+  endDate: string;
+}
+
+export interface AchievementItem {
+  detail: string;
+  link: string;
+}
+
+export interface ProjectItem {
+  name: string;
+  bannerLink: string;
+  repoLink: string;
+  liveLink: string;
+  techStack: string[];
+  detail: string[];
+}
+
+export interface MessageItem {
+  firstName: string;
+  lastName: string;
+  subject: string;
+  email: string;
+  message: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface CertificateItem {
+  name: string;
+  imageLink: string;
+  company: string;
+  certificateLink: string;
+}
+export interface VolunteerItem {
+  name: string;
+  logoLink: string;
+  websiteLink: string;
+  twitterLink: string;
+  linkedinLink: string;
+  detail: string[];
+  startDate: string;
+  endDate: string;
+}
+export interface OpenSourceItem {
+  name: string;
+  logoLink: string;
+  websiteLink: string;
+  twitterLink: string;
+  linkedinLink: string;
+  detail: string[];
+  startDate: string;
+  endDate: string;
+  individualName: string;
+}
+export interface SocialLinkItem {
+  links: [
+    { github: string },
+    { linkedin: string },
+    { twitter: string },
+    { instagram: string },
+    { dev: string },
+    { medium: string },
+    { linktree?: string | null },
+    { facebook: string }
+  ];
+}
+
+export interface MobileIconComponentsItem {
+  [iconName: string]: IconType;
+}
