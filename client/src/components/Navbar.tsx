d71f40149f16d7ee615b7c5ec25f2cf0ab5fb3f7
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-08-12 23:05:58 +0530

    main navbar is updated and made responsive

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,22 +1,68 @@
-import { Link } from "react-router-dom"
-import navItems from "../constants/NavItems"
-
+import { Link } from "react-router-dom";
+import navItems from "../constants/NavItems";
+import "../styles/MobileNavbar.css";
 
 const Navbar = () => {
   return (
-    <div className="navbar" style={{display: "flex", justifyContent: "space-between", boxShadow: "0px 2px 10px #dedede", padding: "1rem 2rem"}}>
-      <div>
-        <Link to="/" style={{fontSize: "1.5rem", textDecoration: "none"}}>Sayan Maity</Link>
-      </div>
+    <div
+      className="navbar"
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        boxShadow: "0px 2px 10px #dedede",
+        padding: "0.5rem 2rem",
+        width: "100%",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          width: "1080px",
+          justifyContent: "space-between",
+        }}
+      >
         <div>
-            {navItems.map((item, index) => {
-                return (
-                    <Link to={item.url} key={index} style={{fontSize: "1rem", textDecoration: "none"}}>{item.label}</Link>
-                )
-            })}
+          <Link
+            to="/"
+            style={{
+              // fontSize: "1.5rem",
+              textDecoration: "none",
+              textTransform: "uppercase",
+            }}
+          >
+            {/* Sayan */}
+            <img
+              src="https://ik.imagekit.io/sayancr777/Portfolio/sayanLogo.jpg?updatedAt=1691434937632"
+              alt="logo"
+              height={40}
+            />
+          </Link>
         </div>
+        <div style={{ display: "flex" }}>
+          {navItems.map((item, index) => {
+            return (
+              <Link
+                to={item.url}
+                key={index}
+                style={{
+                  fontSize: "0.9rem",
+                  textDecoration: "none",
+                  color: "var(--font-color-dark)",
+                  fontWeight: "500",
+                  textTransform: "uppercase",
+                  padding: "0.5rem 1rem",
+                }}
+              >
+                {item.label}
+              </Link>
+            );
+          })}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
