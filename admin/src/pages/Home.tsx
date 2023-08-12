06377bc131d19850aa5586244bb4bd612101ba35
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-08-13 01:24:11 +0530

    home page is created

---
+++ admin/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home Page fill Details</h1>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Home
