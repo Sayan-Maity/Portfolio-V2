3c618c6d25c77456901555f88a593188979faca2
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-08-13 01:05:22 +0530

    blog page of admin created

---
+++ admin/src/pages/Blog.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Blog = () => {
+  return (
+    <div>
+      <h1>Blog Page Fill Details</h1>
+      <Navbar/>
+      
+    </div>
+  )
+}
+
+export default Blog
