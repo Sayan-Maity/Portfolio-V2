1a63231141717d85efdd0803d100693d40df0223
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-08-13 00:24:37 +0530

    the database configuration is shifted to new file

--- server/index.js
+++ server/index.js
@@ -4,9 +4,8 @@ const dotenv = require("dotenv");
 dotenv.config();
 const PORT = process.env.PORT || 3002
 const cors = require("cors")
-const mongoose = require('mongoose')
 const bodyParser = require("body-parser");
-
+const { connectToDatabase } = require("./DB");
 const routes = require("./routes/AllRoutes")
 
 
@@ -19,21 +18,26 @@ app.use("/api", routes)
 
 
 app.get("/", (req, res) => {
-    res.status(200).send("You have landed to Sayan Maity's Portfolio Server !")
+    res.status(200).send("You have landed on Sayan Maity's 👦 Portfolio Server !")
 })
+
 
 
 // Database connection
-mongoose.connect(process.env.MONGODB_URI)
-.then(() => {
-    console.log("Database connected")
-})
-.catch((err) => {
-    console.log(err)
-})
+connectToDatabase().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+});
 
+// mongoose.connect(process.env.MONGODB_URI)
+// .then(() => {
+//     console.log("Database connected")
+// })
+// .catch((err) => {
+//     console.log(err)
+// })
 
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`)
+// })
