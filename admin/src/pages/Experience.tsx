d17f712f8ba807b5363f4c5426008eb4e6f78b25
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-08-13 01:23:55 +0530

    experience page is created

---
+++ admin/src/pages/Experience.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Experience = () => {
+  return (
+    <div>
+      <h1>Experience Page fill Details</h1>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Experience
