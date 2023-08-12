7399491e4a29992e66d6523b630bf2474236cb50
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-08-12 23:09:38 +0530

    sectionTitle is updated and made responsive

--- client/src/components/SectionTitle.tsx
+++ client/src/components/SectionTitle.tsx
@@ -1,32 +1,13 @@
 import { ISectionTitleProps } from "../types/PropTypes";
+import "../styles/SectionTitle.css";
 
-const SectionTitle = ({ mainTitle, subTitle }: ISectionTitleProps) => {
+const SectionTitle = ({ mainTitle, summary }: ISectionTitleProps) => {
   return (
-    <div
-      style={{
-        width: "100%",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        display: "flex",
-        padding: "7rem 1rem",
-      }}
-    >
-      <h1
-        style={{
-          fontSize: "5rem",
-          WebkitTextStroke: "1px #dedede",
-          color: "transparent",
-          position: "absolute",
-          transform: "translateY(-2rem)",
-        }}
-      >
-        {mainTitle}
-      </h1>
-      <div style={{position: "relative"}}>
-        <h1>{mainTitle}</h1>
-        <p>--- {subTitle}</p>
-      </div>
+    <div className="sectionTitle-outerDiv">
+      <h1 className="absoluteTitle">{mainTitle}</h1>
+      <p className="gradient-aurora"></p>
+      <h1 style={{ fontSize: "3rem", position: "relative" }}>{mainTitle}</h1>
+      <p className="sectionTitle-summary">{summary}</p>
     </div>
   );
 };
