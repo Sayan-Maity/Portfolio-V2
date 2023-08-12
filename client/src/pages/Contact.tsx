69724e91717b539242f7c2f7d8558383a5fda987
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-08-12 23:24:34 +0530

    Contact Page is updted and made responsive

--- client/src/pages/Contact.tsx
+++ client/src/pages/Contact.tsx
@@ -1,32 +1,182 @@
 import Footer from "../components/Footer";
 import MobileNavbar from "../components/MobileNavbar";
 import Navbar from "../components/Navbar";
-import Experience from "./Experience";
+import "../styles/ContactPage.css";
+import { FaRegCalendarAlt } from "react-icons/fa";
+import { BiLogoGmail } from "react-icons/bi";
+import ParallaxComponent from "../components/ParallaxComponent";
+import { useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+import FooterGap from "../components/FooterGap";
+import SEO from "../components/SEO";
 
 const Contact = () => {
-  return (
-    <div
-      className="contact"
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
-        height: "100vh",
-      }}
-    >
-      <div>
-        <div>
-          <Navbar />
-        </div>
-        <h1>Contact</h1>
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [subject, setSubject] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
 
-        <div>
-          <MobileNavbar />
+  async function handleSubmitFormBtn(e: React.FormEvent<HTMLFormElement>) {
+    setLoading(true);
+    e.preventDefault();
+    if (!firstName || !lastName || !subject || !email || !message) {
+      toast.error("Please fill all the fields");
+      setLoading(false);
+    } else {
+      if (!email.includes("@")) {
+        toast.error("Please enter a valid email");
+        setLoading(false);
+        return;
+      }
+      try {
+        axios
+          .post("http://localhost:3002/api/postContactForm", {
+            firstName,
+            lastName,
+            subject,
+            email,
+            message,
+          })
+          .then(() => {
+            toast.success("Message sent successfully");
+            setLoading(false);
+            setFirstName("");
+            setLastName("");
+            setSubject("");
+            setEmail("");
+            setMessage("");
+          });
+      } catch (err) {
+        toast.error("Message failed to send");
+        setLoading(false);
+        // console.log("Error: ", err);
+      }
+    }
+  }
+
+  return (
+    <div>
+      <Navbar />
+      <SEO dynamicTitle="Sayan | Contact" />
+      <div style={{ width: "100%", background: "#f4f4f4" }}>
+        <ParallaxComponent
+          title="Contact"
+          wallpaper="https://ik.imagekit.io/sayancr777/tr:w-1000/Portfolio/contactBanner.png?updatedAt=1691398768983"
+        />
+
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <div className="contactFormContainer">
+            <div className="schedule">
+              <div className="emailSchedule">
+                <div className="icon">
+                  <BiLogoGmail />
+                </div>
+                <div className="desc">
+                  <p>sayancr777@gmail.com</p>
+                  <a href="mailto:sayancr777@gmail.com" target="_blank">
+                    <span>Email me</span>
+                  </a>
+                </div>
+              </div>
+              <div className="topmateSchedule">
+                <div className="icon">
+                  <FaRegCalendarAlt />
+                </div>
+                <div className="desc">
+                  <p>Topmate.io</p>
+                  <a
+                    href="https://topmate.io/sayan_maity"
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    <span>Schedule a Meeting</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <form onSubmit={(e) => handleSubmitFormBtn(e)}>
+              {/* <form> */}
+              <div className="rowdivv">
+                <label htmlFor="text">
+                  {" "}
+                  <input
+                    type="text"
+                    name="Name"
+                    id="firstName"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                    placeholder="First Name*"
+                    // required
+                  />
+                </label>
+
+                <label htmlFor="text">
+                  {" "}
+                  <input
+                    type="text"
+                    name="lastName"
+                    id="name"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                    placeholder="Last Name*"
+                    // required
+                  />
+                </label>
+              </div>
+              <label htmlFor="email">
+                {" "}
+                <input
+                  type="email"
+                  name="Email"
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Your Email*"
+                  // required
+                />
+              </label>
+              <label htmlFor="text">
+                <input
+                  type="text"
+                  name="Subject"
+                  id="subject"
+                  value={subject}
+                  onChange={(e) => setSubject(e.target.value)}
+                  placeholder="Subject*"
+                  // required
+                />
+              </label>
+              <label>
+                {" "}
+                <textarea
+                  name="message"
+                  placeholder="Your Message*"
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                ></textarea>
+              </label>
+              <button type="submit">
+                {/* <button onClick={(e) => handleSubmitFormBtn(e)}> */}
+                {loading ? "Please Wait ..." : "Send"}
+              </button>
+            </form>
+          </div>
         </div>
+
+        <div className="body"></div>
       </div>
-      <div>
-        <Footer />
-      </div>
+      <MobileNavbar />
+      <FooterGap/>
+      <Footer />
     </div>
   );
 };
