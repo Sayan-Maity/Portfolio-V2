e7257546ed22e2b1c08dc294b9135c9ad75461a8
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-08-13 19:53:09 +0530

    hotjar fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,10 @@
 import "./App.css";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import SEO from "./components/SEO";
 import { hotjar } from "react-hotjar";
 import { useEffect } from "react";
-import LandingPage from "./pages/LandingPage";
-import Experience from "./pages/Experience";
-import Projects from "./pages/Projects";
-import IndividualProject from "./pages/IndividualProject";
-import Contact from "./pages/Contact";
-import Blogs from "./pages/Blogs";
+import CustomRoutes from "./components/CustomRoutes";
 
 function App() {
   // HotJar Integration :
@@ -25,14 +20,7 @@ function App() {
       <Toaster />
       <Router>
         <SEO dynamicTitle="Sayan | Home" />
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/experience" element={<Experience />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/blogs" element={<Blogs />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/projects/:name" element={<IndividualProject />} />
-        </Routes>
+        <CustomRoutes />
       </Router>
     </>
   );
