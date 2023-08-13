4204911d01e370664fe6108fc0546d96c0f23b03
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-08-13 19:34:20 +0530

    routes were moved back

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,15 @@
 import "./App.css";
-import { BrowserRouter as Router } from "react-router-dom";
-import CustomRoutes from "./components/CustomRoutes";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import SEO from "./components/SEO";
 import { hotjar } from "react-hotjar";
 import { useEffect } from "react";
+import LandingPage from "./pages/LandingPage";
+import Experience from "./pages/Experience";
+import Projects from "./pages/Projects";
+import IndividualProject from "./pages/IndividualProject";
+import Contact from "./pages/Contact";
+import Blogs from "./pages/Blogs";
 
 function App() {
   // HotJar Integration :
@@ -14,13 +19,20 @@ function App() {
       import.meta.env.VITE_HOTJAR_VERSION
     );
   });
-  
+
   return (
     <>
       <Toaster />
       <Router>
         <SEO dynamicTitle="Sayan | Home" />
-        <CustomRoutes />
+        <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/experience" element={<Experience />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/blogs" element={<Blogs />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/projects/:name" element={<IndividualProject />} />
+    </Routes>
       </Router>
     </>
   );
