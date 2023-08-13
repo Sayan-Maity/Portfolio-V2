bb5fa314afb547cfb84e1a21a657b69271b24162
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-08-14 02:21:20 +0530

    all the contents of parallaxItem are made constant

---
+++ client/src/constants/ParallaxItems.tsx
@@ -0,0 +1,26 @@
+import { ParallaxItem } from "../types/FileTypes"
+
+const parallaxItems: ParallaxItem[] = [
+    {
+        title: "Projects",
+        wallpaper: "https://ik.imagekit.io/sayancr777/Portfolio/tr:w-1500/pexels-vincent-ma-janssen-2302802%201.png?updatedAt=1691860720522",
+    },
+    {
+        title: "Experience",
+        wallpaper: "https://ik.imagekit.io/sayancr777/tr:w-1500/Portfolio/experienceBanner.png?updatedAt=1691398885800",
+    },
+    {
+        title: "Contact",
+        wallpaper: "https://ik.imagekit.io/sayancr777/tr:w-1000/Portfolio/contactBanner.png?updatedAt=1691398768983",
+    },
+    {
+        title: "Blogs",
+        wallpaper: "https://ik.imagekit.io/sayancr777/tr:w-1500/Portfolio/blogBanner.png?updatedAt=1691399103917",
+    },
+    {
+        title: "Contact",
+        wallpaper: "https://ik.imagekit.io/sayancr777/tr:w-1000/Portfolio/contactBanner.png?updatedAt=1691398768983",
+    },
+]
+
+export default parallaxItems
