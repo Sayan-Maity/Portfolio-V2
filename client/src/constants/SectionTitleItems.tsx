7d2f61fc9357e4bcc430746bdb4ae8cf81fda0ba
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-08-14 13:19:48 +0530

    sectionTitle is made constant

---
+++ client/src/constants/SectionTitleItems.tsx
@@ -0,0 +1,42 @@
+import { SectionTitleItem } from "../types/FileTypes"
+
+const sectionTitleItems: SectionTitleItem = {
+    aboutMe: {
+        mainTitle: "About Me",
+        summary: "I don't like to brag too much but here's 2 cent of myself I've written, Enjoy reading it 😉"
+    },
+    education: {
+        mainTitle: "Education",
+        summary: "Childhood Memories ."
+    },
+    achievements: {
+        mainTitle: "Achievements",
+        summary: "Showoff Time ;)"
+    },
+    projects: {
+        mainTitle: "Projects",
+        summary: "Take a look at these projects I made in the name of Software Development. Honestly I am quite proud how most of them turned out. Most of them are available on Github so go ahead knock yourself out! The others are client/freelance projects."
+    },
+    nextProject: {
+        mainTitle: "Next Project",
+        summary: "This is not the only project I made, check out this other project that this page randomly chose for you to see, hope you'll like it. And if you don't go back to the projects page to see all the projects I have built so far."
+    },
+    internships: {
+        mainTitle: "Internships",
+        summary: "Showoff Time ;)"
+    },
+    openSource: {
+        mainTitle: "Open Source",
+        summary: "Showoff Time ;)"
+    },
+    volunteering: {
+        mainTitle: "Volunteering",
+        summary: "Showoff Time ;)"
+    },
+    certification: {
+        mainTitle: "Certification",
+        summary: "Showoff Time ;)"
+    },
+}
+
+export default sectionTitleItems
