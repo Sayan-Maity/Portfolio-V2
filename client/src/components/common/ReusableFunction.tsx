7c2f28639017c09f44dd179ed0c4481cbc31cc2c
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-08-15 23:26:52 +0530

    some more reusable functions are added

--- client/src/components/common/ReusableFunction.tsx
+++ client/src/components/common/ReusableFunction.tsx
@@ -1,20 +1,46 @@
 export function formatTimestamp(timestamp: string) {
-    const months = [
-      "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
-    ];
-  
-    const date = new Date(timestamp);
-    const day = date.getDate();
-    const month = months[date.getMonth()];
-    const year = date.getFullYear();
-  
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const amOrPm = hours >= 12 ? "pm" : "am";
-    const formattedHours = hours % 12 || 12;
-    const formattedMinutes = String(minutes).padStart(2, "0");
-  
-    const formattedDate = `${day} ${month}, ${year} at ${formattedHours}:${formattedMinutes} ${amOrPm}`;
-  
-    return formattedDate;
-  }
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const date = new Date(timestamp);
+  const day = date.getDate();
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const amOrPm = hours >= 12 ? "pm" : "am";
+  const formattedHours = hours % 12 || 12;
+  const formattedMinutes = String(minutes).padStart(2, "0");
+
+  const formattedDate = `${month} ${day}`;
+
+  return formattedDate;
+}
+
+export function ToText(node: string) {
+  const tag = document.createElement("div");
+  tag.innerHTML = node;
+  node = tag.innerText;
+  return node;
+}
+
+export function calculateReadingTime(paragraph: string) {
+  const wordCount = paragraph.trim().split(/\s+/).length;
+  const wordsPerMinute = 200;
+  const estimatedTime = Math.ceil(wordCount / wordsPerMinute);
+
+  return estimatedTime;
+}
