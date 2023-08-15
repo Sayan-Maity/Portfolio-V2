0ca01e83cd455fd5ef0a75f39d57715e687ae706
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-08-16 00:48:26 +0530

    small fix

--- client/src/components/common/ReusableFunction.tsx
+++ client/src/components/common/ReusableFunction.tsx
@@ -17,16 +17,11 @@ export function formatTimestamp(timestamp: string) {
   const date = new Date(timestamp);
   const day = date.getDate();
   const month = months[date.getMonth()];
-  const year = date.getFullYear();
 
   const hours = date.getHours();
   const minutes = date.getMinutes();
-  const amOrPm = hours >= 12 ? "pm" : "am";
-  const formattedHours = hours % 12 || 12;
-  const formattedMinutes = String(minutes).padStart(2, "0");
 
   const formattedDate = `${month} ${day}`;
-
   return formattedDate;
 }
 
