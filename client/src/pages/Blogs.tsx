09b9d01b41063673e934920a2a5ca16665c6db1b
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-08-16 16:59:36 +0530

    skeleton is added in blogs

--- client/src/pages/Blogs.tsx
+++ client/src/pages/Blogs.tsx
@@ -16,23 +16,29 @@ import { Link } from "react-router-dom";
 import { BsLink45Deg } from "react-icons/bs";
 import SectionTitle from "../components/SectionTitle";
 import sectionTitleItems from "../constants/SectionTitleItems";
+import LoadingSkeleton from "../components/LoadingSkeleton";
 
 const Contact = () => {
   const [mediumBlogInfo, setMediumBlogInfo] = useState<any>({});
   const [devToBlogInfo, setDevToBlogInfo] = useState<any>({});
+  const [isLoadingMediumBlog, setIsLoadingMediumBlog] = useState<boolean>(true);
+  const [isLoadingDevBlog, setIsLoadingDevBlog] = useState<boolean>(true);
 
   useEffect(() => {
+    // setTimeout(() => {
     axios
       .get(
         "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@Sayancr777"
       )
       .then((res) => {
         // console.log(res.data);
+        setIsLoadingMediumBlog(false);
         setMediumBlogInfo(res.data);
       })
       .catch((err) => {
         console.log(err);
       });
+    // }, 5000);
   }, []);
 
   useEffect(() => {
@@ -60,72 +66,89 @@ const Contact = () => {
           title={parallaxItems[3].title}
           wallpaper={parallaxItems[3].wallpaper}
         />
-        <SectionTitle mainTitle={sectionTitleItems?.blog?.mainTitle} summary={sectionTitleItems?.blog?.summary} />
+        <SectionTitle
+          mainTitle={sectionTitleItems?.blog?.mainTitle}
+          summary={sectionTitleItems?.blog?.summary}
+        />
         <div className="blog_outerDiv">
-          {mediumBlogInfo?.items?.map((item: any, index: number) => (
-            <div className="blog_innerDiv" key={index}>
-              <div className="blog-left">
-                <img src={item?.thumbnail} alt="" />
-              </div>
-              <div className="blog-right">
-                <div className="blog-title-source">
-                  <Link to={item.link} target="_blank">
-                    <p className="blog-title">
-                      <BsLink45Deg /> {item.title}
+          {isLoadingMediumBlog && mediumBlogInfo?.items?.lenght > 0 && (
+            <LoadingSkeleton h1Count={1} pCount={3} circleCount={1} />
+          )}
+          {!isLoadingMediumBlog &&
+            mediumBlogInfo?.items?.map((item: any, index: number) => (
+              <div className="blog_innerDiv" key={index}>
+                <div className="blog-left">
+                  <img src={item?.thumbnail} alt="" />
+                </div>
+                <div className="blog-right">
+                  <div className="blog-title-source">
+                    <Link to={item.link} target="_blank">
+                      <p className="blog-title">
+                        <BsLink45Deg /> {item.title}
+                      </p>
+                    </Link>
+                    <p className="blog-source">Medium</p>
+                  </div>
+                  <p>
+                    {ToText(item?.description.substring(0, 250))}...{" "}
+                    <Link to={item.link} target="_blank">
+                      read more
+                    </Link>{" "}
+                  </p>
+                  <div className="time-date">
+                    <p className="blog-readTime">
+                      {calculateReadingTime(ToText(item?.description))} min read
                     </p>
-                  </Link>
-                  <p className="blog-source">Medium</p>
+                    <p className="blog-date">{formatTimestamp(item.pubDate)}</p>
+                  </div>
+                  {/* {item?.categories.length > 0 && <p>{item.categories[0]}</p>} */}
                 </div>
-                <p>{ToText(item?.description.substring(0, 250))}... <Link to={item.link} target="_blank">read more</Link> </p>
-                <div className="time-date">
-                  <p className="blog-readTime">
-                    {calculateReadingTime(ToText(item?.description))} min read
-                  </p>
-                  <p className="blog-date">{formatTimestamp(item.pubDate)}</p>
+              </div>
+            ))}
+
+          {isLoadingDevBlog && devToBlogInfo?.result?.length > 0 && (
+            <LoadingSkeleton h1Count={1} pCount={3} circleCount={1} />
+          )}
+          {!isLoadingDevBlog &&
+            devToBlogInfo?.result?.map((item: any, index: number) => (
+              <div className="blog_innerDiv" key={index}>
+                <div className="blog-left">
+                  <img src={item?.cover_image} alt="" />
                 </div>
-                {/* {item?.categories.length > 0 && <p>{item.categories[0]}</p>} */}
+                <div className="blog-right">
+                  <div className="blog-title-source">
+                    <Link to={`https://dev.to/${item?.path}`} target="_blank">
+                      <p className="blog-title">
+                        <BsLink45Deg /> {item.title}
+                      </p>
+                    </Link>
+                    <p className="blog-source">Dev.to</p>
+                  </div>
+                  {/* <p>{ToText(item?.description.substring(0, 250))}... <Link to={`https://dev.to/${item?.path}`} target="_blank">read more</Link> </p> */}
+                  <div className="time-date">
+                    <p className="blog-readTime">
+                      {item?.reading_time} min read
+                    </p>
+                    <p className="blog-date">
+                      {formatTimestamp(item.readable_publish_date)}
+                    </p>
+                  </div>
+                  {/* {item?.categories.length > 0 && <p>{item.categories[0]}</p>} */}
+                </div>
               </div>
-            </div>
-          ))}
 
-          {devToBlogInfo?.result?.map((item: any, index: number) => (
-
-            <div className="blog_innerDiv" key={index}>
-            <div className="blog-left">
-              <img src={item?.cover_image} alt="" />
-            </div>
-            <div className="blog-right">
-              <div className="blog-title-source">
-                <Link to={`https://dev.to/${item?.path}`} target="_blank">
-                  <p className="blog-title">
-                    <BsLink45Deg /> {item.title}
-                  </p>
-                </Link>
-                <p className="blog-source">Dev.to</p>
-              </div>
-              {/* <p>{ToText(item?.description.substring(0, 250))}... <Link to={`https://dev.to/${item?.path}`} target="_blank">read more</Link> </p> */}
-              <div className="time-date">
-                <p className="blog-readTime">
-                  {item?.reading_time} min read
-                </p>
-                <p className="blog-date">{formatTimestamp(item.readable_publish_date)}</p>
-              </div>
-              {/* {item?.categories.length > 0 && <p>{item.categories[0]}</p>} */}
-            </div>
-            </div>
-
-            // <div className="blog_innerDiv" key={index}>
-            //   <Link to={`https://dev.to/${item?.path}`} target="_blank">
-            //     <h1>{item?.title}</h1>
-            //   </Link>
-            //   <p>Dev.to</p>
-            //   <p>{item?.published_timestamp}</p>
-            //   <p>Pub Date - {item?.readable_publish_date}</p>
-            //   <p>{item?.reading_time} min read</p>
-            //   <img src={item?.cover_image} alt="" />
-            //   {item?.tag_list.length > 0 && <p>{item?.tag_list[0]}</p>}
-            // </div>
-          ))}
+              // <div className="blog_innerDiv" key={index}>
+              //   <Link to={`https://dev.to/${item?.path}`} target="_blank">
+              //     <h1>{item?.title}</h1>
+              //   </Link>
+              //   <p>Dev.to</p>
+              //   <p>{item?.published_timestamp}</p>
+              //   <p>Pub Date - {item?.readable_publish_date}</p>
+              //   <p>{item?.reading_time} min read</p>
+              //   <img src={item?.cover_image} alt="" />
+              //   {item?.tag_list.length > 0 && <p>{item?.tag_list[0]}</p>}
+              // </div>
+            ))}
         </div>
       </div>
       <MobileNavbar />
