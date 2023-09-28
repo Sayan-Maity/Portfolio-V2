5b6945da0d1b5d48cfbb419f17c29e06d68f0c4a
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-09-28 23:52:43 +0530

    updated the endpoint

--- client/src/components/ChatBot.tsx
+++ client/src/components/ChatBot.tsx
@@ -73,7 +73,7 @@ const ChatBot = () => {
         try {
             setLoading(true);
             const response = await axios.post(
-                `${import.meta.env.VITE_SERVER_URL}/api/chatCompletion`,
+                `${import.meta.env.VITE_SERVER_URL}/chatCompletion`,
                 // `http://localhost:3002/api/chatCompletion`,
                 {
                     userPrompt: message,
